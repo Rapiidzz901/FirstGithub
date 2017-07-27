5383a9e87889b365d6ca253ff7bc5cd5367dc068
BRANDON\brandon <rapiidzz94@gmail.com>
2017-07-27 13:53:27 +0100

    Initial commit

---
+++ Source/FirstGithub/FirstGithubGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "FirstGithubGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTGITHUB_API AFirstGithubGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
